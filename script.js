d7a3c2d4fe04e58aaa156d6dc36a6a6a0a901c74
Nova Amiko <beckhv2@gmail.com>
2023-05-07 11:45:47 -0800

    added documentation to functions

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function convertNumToHex(decimalNumber) {
     }
 }
 
-function createNewHex() {
+function randomHex() {
     let newHex = "#";
     for (let i = 0; i < 6; i++) {
         let newValue = Math.floor(Math.random() * 16); //number [0, 15]
@@ -26,58 +26,31 @@ function createNewHex() {
     return newHex;
 }
 
-function createPaletteList(paletteSize) {
+function createPaletteList(paletteSize, f) {
     let paletteList = [];
     for (let i = 0; i < paletteSize; i++) {
-        paletteList.push(createNewHex());
+        paletteList.push(f());
     }
     return paletteList;
 }
 
+
+/**
+ * Updates the number displayed next to the slider
+ * @param {number} sliderVal - value collected from the slider
+ */
 function updateVal(sliderVal) {
     document.getElementById('slider-int').innerHTML = sliderVal;
 }
 
-function countSquares() {
-    const squareCount = document.getElementsByClassName('square');
-    return squareCount.length;
-}
 
-function getRows(items) {
-    if (items > 5) {
-        return Math.ceil(items / 5);
-    } else {
-        return 1;
-    };
-}
-
-function getColumns(items) {
-    if (items >= 5) {
-        return 5;
-    } else {
-        return items;
-    };
-}
-
-function makeSquare(color) {
-    const square = document.createElement('div');
-    square.innerHTML = color;
-    square.style.backgroundColor = color;
-    square.setAttribute('class', 'square');
-    square.setAttribute('id', `square-${countSquares() + 1}`);
-    document.getElementById('display-area').appendChild(square);
-}
-
-function resetScreen(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild)
-    };
-}
-
+/**
+ * makes a random color palette and displays it in the 'display-area'
+ */
 function randomPalette() {
     resetScreen(document.getElementById('display-area'));
     const sliderVal = document.getElementById('slider').value;
-    const paletteList = createPaletteList(sliderVal);
+    const paletteList = createPaletteList(sliderVal, randomHex);
     for (let p of paletteList) {
         makeSquare(p);
     };
@@ -87,5 +60,66 @@ function randomPalette() {
     for (let s of (document.getElementsByClassName('square'))) {
         s.style.width = `${widthPercent.toString()}%`;
         s.style.height = `${heightPercent.toString()}%`;
-    };  
+    }; 
+
+    /**
+     * @returns number of color squares in the display area
+     */
+    function countSquares() {
+        const squareCount = document.getElementsByClassName('square');
+        return squareCount.length;
+    }
+
+
+    /**
+     * calculates how many rows are needed to display the colors
+     * @param {number} squares - number of squares in the display area
+     * @returns number 1 through 5
+     */
+    function getRows(squares) {
+        if (squares > 5) {
+            return Math.ceil(squares / 5);
+        } else {
+            return 1;
+        };
+    }
+    
+
+    /**
+     * calculates how many columns are needed to display the colors
+     * @param {number} squares - number of squares in the display area
+     * @returns number 1 through 5
+     */
+    function getColumns(squares) {
+        if (squares >= 5) {
+            return 5;
+        } else {
+            return squares;
+        };
+    }
+
+
+    /**
+     * makes a color square in the display area
+     * @param {string} color - a single hex value for a color
+     */
+    function makeSquare(color) {
+        const square = document.createElement('div');
+        square.innerHTML = color;
+        square.style.backgroundColor = color;
+        square.setAttribute('class', 'square');
+        square.setAttribute('id', `square-${countSquares() + 1}`);
+        document.getElementById('display-area').appendChild(square);
+    }
+
+
+    /**
+     * resets the screen to remove any color squares 
+     * @param {*} parent - the display area where the color squares appear
+     */
+    function resetScreen(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild)
+        };
+    }
 }
